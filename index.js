9ce48c66e93a9869d2545d0b43f2a723f7afedec
Adiar <datkaevadiar@gmail.com>
2024-06-01 10:42:16 +0500

    feat: people endpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('Hello world');
+app.get('/people', async (req, res) => {
+	try {
+		const response = await axios.get('https://swapi.dev/api/people/?page=1');
+		res.json(response.data);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Error calling external API');
+	}
 });
 
 app.listen(4444, err => {
